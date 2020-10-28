417b9c7233fae3c908572d25a95c5a5a9274391f
Sam Khanal <samkhanal@iclooud.com>
2020-10-28 17:28:25 -0400

    API calls working perfectly

--- bet.js
+++ bet.js
@@ -1,43 +1,29 @@
-//     url: "https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/predict?team2=chelsea&team1=everton",
-
-let gamePrediction, userInput;
-//Game predition and userinput
-
-const $team1 = $('#team1');
-const $team2 = $('#team2');
-const $result = $('#result');
-const $input = $('input[type="text"]');
-//Populate the dom with userinput for home team and away team with each as team1 and team2
-
-$('form').on('submit', handleGetData);
-
 const api_key= 'e47b52639fmsh8da4044912b4134p1c3797jsnd682dda7cb43'
-// Const api key to be used, set as a const
 
-//This is to prevent the refresh of the page
-function handleGetData(event) {
+document.addEventListener('submit', function (event) {
     event.preventDefault();
-    userInput = $input.val();
-    axios.get("https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/predict?team2='+ userInput'&team1='+ userInput'", {
+
+    let team1 = document.querySelector('#team2').value;
+    let team2 = document.querySelector('#team1').value;
+
+    axios.get("https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/predict", {
     headers: {
         "x-rapidapi-host": "heisenbug-premier-league-live-scores-v1.p.rapidapi.com",
         "x-rapidapi-key": api_key
+    },
+    params: {
+        team1: team1,
+        team2: team2
     }
-})
-.then(response => {
-    console.log(JSON.stringify(response.data.team1))
-    console.log(JSON.stringify(response.data.team2))
-    console.log(JSON.stringify(response.data.result))
-    gamePrediction = response;
-    render();
-})
-}
-
-function render() {
-    $team1.text(response.data.team1);
-    $team2.text(response.data.team2);
-    $result.text(response.data.result);
-}
+    })
+    .then(response => {
+        console.log(team1)
+        console.log(team2)
+        console.log(JSON.stringify(response.data.team1))
+        console.log(JSON.stringify(response.data.team2))
+        console.log(JSON.stringify(response.data.result))
+    })
+});
 
 
 
