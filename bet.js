4a54c82fd1a5a972569bd6801bfc907ac120b076
Sam Khanal <samkhanal@iclooud.com>
2020-10-28 20:28:34 -0400

    Project

--- bet.js
+++ bet.js
@@ -1,11 +1,10 @@
-const api_key= 'e47b52639fmsh8da4044912b4134p1c3797jsnd682dda7cb43'
-
 document.addEventListener('submit', function (event) {
     event.preventDefault();
 
     let team1 = document.querySelector('#team2').value;
     let team2 = document.querySelector('#team1').value;
 
+    const api_key= 'e47b52639fmsh8da4044912b4134p1c3797jsnd682dda7cb43'
     axios.get("https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/predict", {
     headers: {
         "x-rapidapi-host": "heisenbug-premier-league-live-scores-v1.p.rapidapi.com",
@@ -22,9 +21,21 @@ document.addEventListener('submit', function (event) {
         console.log(JSON.stringify(response.data.team1))
         console.log(JSON.stringify(response.data.team2))
         console.log(JSON.stringify(response.data.result))
+        localStorage.setItem('bettingData', JSON.stringify(response.data));
+
+        window.location = './result.html';
     })
 });
 
+
+
+// Get data
+var data = localStorage.getItem('bettingData');
+
+console.log(data);
+if (data) {
+	document.getElementById('bettingResult').innerText = data;
+}
 
 
 
